5e754ee6efca1bac8d425a81a1d092eb0c392904
unknown <bennattj@gmail.com>
2023-08-05 01:25:28 -0500

    Added homePage (which is to my website)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-// import data from './half-ppr.json'
-import PlayerDisplay from './components/PlayerDisplay/PlayerDisplay'
-import ScrollableList from './components/ScrollableList/ScrollableList';
-import { Container, Row, Col } from 'react-bootstrap';
 import PlayerMultiDisplay from './components/PlayerMultiDisplay/PlayerMultiDisplay';
 import './App.css'
 
