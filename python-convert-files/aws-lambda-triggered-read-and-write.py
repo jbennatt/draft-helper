9a4600dc1d13ddb1117ad33bb5bc9d225ee6272f
unknown <bennattj@gmail.com>
2023-08-10 03:12:55 -0500

    Updated lambda function. Updated .gitignore to ignore data folder (in root folder).

--- python-convert-files/aws-lambda-triggered-read-and-write.py
+++ python-convert-files/aws-lambda-triggered-read-and-write.py
@@ -4,61 +4,57 @@ import urllib.parse
 import boto3
 
 
-def convertCsvToJsonArray(csvDict):
-    jsonArr = []
-
-    for row in csvDict:
-        jsonArr.append(row)
-
-    return jsonArr
-
-
-def reMapPlayerKeys(player):
-    newPlayer = {}
-    # print(f're-mapping {player}')
-    for playerKey in player.keys():
-        # print(f're-mapping {playerKey} to {"_".join(playerKey.lower().split())}')
-        newPlayer['_'.join(playerKey.lower().split())] = player[playerKey]
-    return newPlayer
-
-
-def reMapJsonArray(jsonArr):
-    return [reMapPlayerKeys(player) for player in jsonArr]
-
-
-s3 = boto3.client('s3')
-
 jsonBucket = "jaredbennatt.com"
 jsonPath = 'draft-helper/data/json/rankings.json'
 
 
 def lambda_handler(event, context):
+    print(json.dumps(event, indent=2))
+    s3 = boto3.client('s3')
     firstS3Record = event['Records'][0]['s3']
     bucket = firstS3Record['bucket']['name']
     key = urllib.parse.unquote_plus(firstS3Record['object']['key'], 'utf-8')
-
+    
     print(f'trying to fetch: {bucket}/{key}')
-
+    
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
         print(f"CONTENT TYPE: {response['ContentType']}")
-
+        
         csvDict = csv.DictReader(response['Body'].read().decode('utf-8').splitlines(True))
-        badJson = convertCsvToJsonArray(csvDict)
-        goodJson = reMapJsonArray(badJson)
-
-        print(f'first: {goodJson[0]}')
-        print(f'last: {goodJson[-1]}')
-
+        jsonReMapped = reMapJsonArray(csvDict)
+        
+        print(f'first: {jsonReMapped[0]}')
+        print(f'last: {jsonReMapped[-1]}')
+        
         print(f'trying to put json into {jsonBucket}/{jsonPath}')
-
+        
+        # requires permission: Action s3:PutObject
         s3.put_object(
-            Body=json.dumps(goodJson, indent=2).encode('utf-8'),
+            ACL='public-read', # requires permission: Action s3:PutObjectAcl
+            Body=json.dumps(jsonReMapped, indent=2).encode('utf-8'),
             Bucket=jsonBucket,
-            Key=jsonPath
-        )
+            Key=jsonPath,
+            ContentType='application/json'
+            )
         return "SUCCESS"
     except Exception as e:
         print (e)
         print(f'Error getting object {key} from bucket {bucket}')
         raise e
+
+
+def reMapPlayerKeys(player):
+    newPlayer = {}
+
+    # iterate over keys in each player object
+    # remove all spaces by splitting the key on whitespace then joining the 
+    #   tokens with an underscore
+    # update the empty newPlayer object with the "good" key to the "bad" key's value
+    for playerKey in player.keys():
+        newPlayer['_'.join(playerKey.lower().split())] = player[playerKey]
+    return newPlayer
+
+
+def reMapJsonArray(jsonArr):
+    return [reMapPlayerKeys(player) for player in jsonArr]  
